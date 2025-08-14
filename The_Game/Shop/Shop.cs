cfddcba0b076a4a88dd1bd98509c930bc8434a09
Mario Dickerson <dai.dickerson@gmail.com>
2025-08-14 10:41:35 -0500

    branch created
    Add Shop and ShopItem classes to Shop namespace
    
    Introduces `Shop` and `ShopItem` classes in the `The_Game.Shop` namespace.
    The `Shop` class manages a `CharacterBase` and a list of `ShopItem` objects.
    The `ShopItem` class is abstract, featuring properties for `Id`, `Name`,
    `Cost`, `ExpCost`, and `Description`, with a constructor that assigns a
    unique `Id` using a static counter.

---
+++ The_Game/Shop/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Game.character;
+
+namespace The_Game.Shop
+{
+    public class Shop
+    {
+        public CharacterBase Character { get; set; }
+
+        public List<ShopItem> Items { get; set; }
+
+        public Shop() { }
+    }
+}
---
+++ The_Game/Shop/ShopItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Game.Shop
+{
+    public abstract class ShopItem
+    {
+        private static int nextId = 0;
+        public int Id { get; private set; }
+
+        public string Name { get; set; }
+        public int? Cost { get; set; }
+        public int? ExpCost { get; set; }
+        public string? Description { get; set; }
+
+        public ShopItem(string name, int? cost, int? expCost, string? description) 
+        {
+            Name = name;
+            Cost = cost;
+            ExpCost = expCost;
+            Description = description;
+            Id = nextId++;
+
+            
+
+        }
+    }
+}
